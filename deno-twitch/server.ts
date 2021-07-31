3f70c57a04d361f932a18536d99558a2e96d4356
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2021-07-31 16:55:24 +0900

    Add simple types for server

--- deno-twitch/server.ts
+++ deno-twitch/server.ts
@@ -7,10 +7,18 @@ import { hmac } from "https://deno.land/x/hmac@v2.0.1/mod.ts";
 
 const twitchSigningSecret = Deno.env.get("TWITCH_SIGNING_SECRET");
 
+type PostHandler = (req: ServerRequest) => void;
+type GetHandler = (req: ServerRequest) => void;
+
+interface FetchEvent {
+    request: Request;
+    respondWith: (response: Response) => void;
+}
+
 class Application {
-    private getters: Map<string, (req: ServerRequest) => void>;
-    private posters: Map<string, (req: ServerRequest) => void>;
     private middlewares: Array<(req: ServerRequest) => Promise<void>>;
+    private getters: Map<string, GetHandler>;
+    private posters: Map<string, PostHandler>;
 
     constructor() {
         this.getters = new Map();
@@ -22,11 +30,11 @@ class Application {
         this.middlewares.push(handler);
     }
 
-    get(path: string, handler: (req: ServerRequest) => void) {
+    get(path: string, handler: GetHandler) {
         this.getters.set(path, handler);
     }
 
-    post(path: string, handler: (req: ServerRequest) => void) {
+    post(path: string, handler: PostHandler) {
         this.posters.set(path, handler);
     }
 
@@ -36,14 +44,16 @@ class Application {
             const { method, url } = req;
             const [path] = url.split("?");
 
+            /*
             for await (const middleware of this.middlewares) {
                 await middleware(req);
             }
+            */
 
             switch (method) {
                 case "GET": {
                     if (this.getters.has(path)) {
-                        const func = this.getters.get(path) as Function;
+                        const func = this.getters.get(path) as GetHandler;
                         func(req);
                     } else {
                         req.respond({ status: 404 });
@@ -52,7 +62,7 @@ class Application {
                 }
                 case "POST": {
                     if (this.posters.has(path)) {
-                        const func = this.posters.get(path) as Function;
+                        const func = this.posters.get(path) as PostHandler;
                         func(req);
                     } else {
                         req.respond({ status: 404 });
