19d5bf6f81093fb356349d3b2d2cead78ff92e7f
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2021-09-07 16:17:31 +0900

    Send HTML on /

--- deno-twitch/main.ts
+++ deno-twitch/main.ts
@@ -79,7 +79,28 @@ app.use(async (req) => {
 });
 
 app.get("/", (req) => {
-    req.respond({ status: 200, body: "Hello World!" });
+    const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joxtabot</title>
+</head>
+<body>
+    <h1>Welcome to Joxtabot</h1>
+</body>
+</html>
+`;
+    const headers = new Headers({
+        "Content-Type": "text/html; charset=UTF-8",
+    });
+    req.respond({
+        status: 200,
+        body: html,
+        headers,
+    });
 });
 
 app.get("/hello", (req) => {
