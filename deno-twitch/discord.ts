06f015a2bc3202921ee0d8dd2c93f2732d6fc2b5
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2021-09-10 11:38:11 +0900

    Extract Discord code to separate file

---
+++ deno-twitch/discord.ts
@@ -0,0 +1,40 @@
+import { sendMessage } from "./deps.ts";
+
+const joxtacyIsLiveChannelId = BigInt(Deno.env.get("DISCORD_CHANNEL_ID") || 0);
+
+export const sendOnlineNotification = async (event: any) => {
+    let title = "Le title";
+    let gameName = "Le game";
+    try {
+        const yep = await fetch(
+            `https://api.twitch.tv/helix/streams?user_id=${54605357}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${Deno.env.get("TWITCH_APP_TOKEN")}`,
+                    "Client-Id": `${Deno.env.get("TWITCH_CLIENT_ID")}`,
+                },
+            }
+        );
+        const { data } = await yep.json();
+        title = data[0].title;
+        gameName = data[0].game_name;
+        console.log("Twitch stream info", data);
+    } catch (_error) {
+        // noop
+    }
+
+    const messageContent = `
+**Hi everyone! I am live!**
+> Playing: ${gameName}
+> Title: ${title}
+https://twitch.tv/joxtacy
+`;
+
+    const discordMessage = await sendMessage(
+        joxtacyIsLiveChannelId,
+        messageContent
+    );
+    console.log(
+        `Stream online notification for ${event.broadcaster_user_name} sent. Message ID: ${discordMessage.id}`
+    );
+};
