7dd5196b9641c503e4e3375fdb0c8d79af883272
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2021-10-13 21:43:57 +0900

    client: First commit

---
+++ client/src/routes/__error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+  export function load({ error, status }: { error: Error; status: number }) {
+    return {
+      props: {
+        error,
+        status,
+      },
+    };
+  }
+</script>
+
+<script lang="ts">
+  export let error: Error;
+  export let status: number;
+</script>
+
+<h1>rip {status}</h1>
+<h2>{error.name} - {error.message}</h2>
+<h3>{error.stack}</h3>
---
+++ client/src/routes/settings.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count = 0;
+
+  const increment = () => {
+    count++;
+  };
+</script>
+
+<h1>Bajsballe {count}</h1>
+<button on:click={increment}>bajsballe</button>
