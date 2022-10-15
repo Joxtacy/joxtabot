687d6d2cdaa7a02bcd74dd287805593a2fc38c03
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2022-10-15 11:29:05 +0900

    rust-server: server: extract examples to README.md

--- rust-server/server/src/main.rs
+++ rust-server/server/src/main.rs
@@ -23,90 +23,16 @@ mod twitch {
 
     use crate::websocket_utils::TwitchCommand;
 
-    // Webhook subscription verification challenge example
-    // {
-    //   "challenge": "pogchamp-kappa-360noscope-vohiyo",
-    //   "subscription": {
-    //     "id": "f1c2a387-161a-49f9-a165-0f21d7a4e1c4",
-    //     "status": "webhook_callback_verification_pending",
-    //     "type": "channel.follow",
-    //     "version": "1",
-    //     "cost": 1,
-    //     "condition": {
-    //       "broadcaster_user_id": "12826"
-    //     },
-    //     "transport": {
-    //       "method": "webhook",
-    //       "callback": "https://example.com/webhooks/callback"
-    //     },
-    //     "created_at": "2019-11-16T10:11:12.123Z"
-    //   }
-    // }
     #[derive(Serialize, Deserialize, Debug)]
     pub struct VerificationChallenge {
         pub challenge: String,
         subscription: Subscription,
     }
 
-    // Webhook subscription revoked example
-    // {
-    //   "subscription": {
-    //     "id": "f1c2a387-161a-49f9-a165-0f21d7a4e1c4",
-    //     "status": "authorization_revoked",
-    //     "type": "channel.follow",
-    //     "cost": 1,
-    //     "version": "1",
-    //     "condition": {
-    //       "broadcaster_user_id": "12826"
-    //     },
-    //     "transport": {
-    //       "method": "webhook",
-    //       "callback": "https://example.com/webhooks/callback"
-    //     },
-    //     "created_at": "2019-11-16T10:11:12.123Z"
-    //   }
-    // }
     #[derive(Serialize, Deserialize, Debug)]
     pub struct RevokedSubscription {
         pub subscription: Subscription,
     }
-
-    // Redeem reward example.
-    // {
-    //     "subscription": {
-    //         "id": "62a04e8f-5c47-acfa-6895-274d679abf85",
-    //&         "status": "enabled",
-    //         "type": "channel.channel_points_custom_reward_redemption.add",
-    //         "version": "1",
-    //         "condition": {
-    //             "broadcaster_user_id": "54605357"
-    //         },
-    //         "transport": {
-    //             "method": "webhook",
-    //             "callback": "null"
-    //         },
-    //         "created_at": "2022-07-13T04:03:22.577859Z",
-    //         "cost": 0
-    //     },
-    //     "event": {
-    //         "id": "62a04e8f-5c47-acfa-6895-274d679abf85",
-    //         "broadcaster_user_id": "54605357",
-    //         "broadcaster_user_login": "testBroadcaster",
-    //         "broadcaster_user_name": "testBroadcaster",
-    //         "user_id": "91240126",
-    //         "user_login": "testFromUser",
-    //         "user_name": "testFromUser",
-    //         "user_input": "Test Input From CLI",
-    //         "status": "unfulfilled",
-    //         "reward": {
-    //             "id": "c3f5c149-65e9-4bc0-2271-723f41731542",
-    //             "title": "Test Reward from CLI",
-    //             "cost": 150,
-    //             "prompt": "Redeem Your Test Reward from CLI"
-    //         },
-    //         "redeemed_at": "2022-07-13T04:03:22.577859Z"
-    //     }
-    // }
 
     #[derive(Serialize, Deserialize, Debug)]
     pub struct Condition {
