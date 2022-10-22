457740a859cb128929076a3a78cc322ee86b7e1e
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2022-10-22 12:35:38 +0900

    rust-server: server: add string-utils tests

--- rust-server/server/src/string_utils/mod.rs
+++ rust-server/server/src/string_utils/mod.rs
@@ -17,3 +17,29 @@ pub fn create_stream_online_message(game: &str, title: &str) -> String {
 pub fn create_privmsg(channel: &str, message: &str) -> String {
     format!("PRIVMSG #{} :{}", channel, message)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{create_privmsg, create_stream_online_message};
+
+    #[test]
+    fn test_create_privmsg() {
+        let result = create_privmsg("My_Channel", "My Message");
+
+        let expected = "PRIVMSG #My_Channel :My Message";
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_create_stream_online_message() {
+        let result = create_stream_online_message("Rust", "Playing Game With Toxic Gamers");
+
+        let expected = "Hi @everyone! I am **live**!\n\
+                            > Playing: Rust\n\
+                            > Title: Playing Game With Toxic Gamers\n\
+                            https://twitch.tv/joxtacy";
+
+        assert_eq!(result, expected);
+    }
+}
