09853ee0b371c2c9c03069a90a53d10b6e49f01c
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2022-11-26 11:52:02 +0900

    feat(rust-server): add logging to `twitch-utils`

--- rust-server/twitch-utils/src/lib.rs
+++ rust-server/twitch-utils/src/lib.rs
@@ -1,3 +1,4 @@
+use log::{debug, error, info, warn};
 use reqwest::StatusCode;
 use serde::Deserialize;
 
@@ -33,6 +34,7 @@ pub async fn get_stream_info(
     client_id: String,
     user_id: u64,
 ) -> Result<StreamInfo, String> {
+    info!(target: "TWITCH_UTILS", "Getting stream info for user: {}", user_id);
     let url = format!("https://api.twitch.tv/helix/streams?user_id={}", user_id);
     let client = reqwest::Client::new();
 
@@ -45,22 +47,30 @@ pub async fn get_stream_info(
 
     match resp {
         Ok(response) => {
+            debug!(target: "TWITCH_UTILS", "Got response: {:?}", response);
             let status_code = response.status();
             if StatusCode::is_success(&status_code) {
-                // println!("RESULT: {}", response.text().await.unwrap());
-
+                debug!(target: "TWITCH_UTILS", "Got stream info");
                 let res = response.json::<StreamInfo>().await;
                 match res {
                     Ok(stream_info) => Ok(stream_info),
-                    Err(e) => Err(format!("Failed to get stream info. Reason: {:?}", e)),
+                    Err(e) => {
+                        warn!(target: "TWITCH_UTILS", "Failed parsing response body. Reason: {}", e.to_string());
+                        Err(format!("Failed to get stream info. Reason: {:?}", e))
+                    }
                 }
             } else {
+                let response_text = response.text().await.unwrap_or_else(|err| err.to_string());
+                warn!(target: "TWITCH_UTILS", "Failed getting stream info. Reason: {}", response_text);
                 Err(format!(
-                    "Failed to get stream info. Reason: {:?}",
-                    response.text().await
+                    "Failed to get stream info. Reason: {}",
+                    response_text
                 ))
             }
         }
-        Err(error) => Err(format!("Failed to get stream info. Reason: {:?}", error)),
+        Err(error) => {
+            error!(target: "TWITCH_UTILS", "Error when sending request. Reason: {:?}", error);
+            Err(format!("Failed to get stream info. Reason: {:?}", error))
+        }
     }
 }
