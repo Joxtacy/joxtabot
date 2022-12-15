3bbffa5c1a806c7c8e07ac02d4ec18f9f2b4421f
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2022-12-15 16:56:04 +0900

    chore(sveltekit-client): svelte-migrate - fix `@migration`s

--- sveltekit-client/src/routes/+error.svelte
+++ sveltekit-client/src/routes/+error.svelte
@@ -1,24 +1,10 @@
-<script context="module" lang="ts">
-  import type { ErrorLoad } from "@sveltejs/kit";
-
-  export const load: ErrorLoad = ({ error, status }) => {
-    return {
-      props: {
-        error,
-        status,
-      },
-    };
-  };
-</script>
-
 <script lang="ts">
-  export let error: Error;
-  export let status: number;
+  import { page } from "$app/stores";
 
-  console.log("error", error);
-  console.log("status", status);
+  console.log("error", page.error);
+  console.log("status", page.status);
 </script>
 
-<img src="https://http.cat/{status}" alt={`Status code: ${status}`} />
-<h1>Oh noes! {status}</h1>
-<h2>{error.message}</h2>
+<img src="https://http.cat/{$page.status}" alt={`Status code: ${$page.status}`} />
+<h1>Oh noes! {$page.status}</h1>
+<h2>{$page.error.message}</h2>
