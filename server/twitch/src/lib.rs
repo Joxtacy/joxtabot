fd467a936df52fb3b6931ad8fafdbc3b0fd141b4
Jesper Hasselquist <jesper.hasselquist@pm.me>
2023-07-11 15:58:20 +0200

    feat(twitch): add basic Twitch lib

---
+++ server/twitch/src/lib.rs
@@ -0,0 +1,228 @@
+use log::{debug, error, info, warn};
+use reqwest::{Client, StatusCode};
+use serde::Deserialize;
+
+const TARGET: &str = "TWITCH";
+
+pub struct Twitch {
+    /// Bearer token used for authentication of the Discord API
+    token: String,
+    /// Client ID of your registered bot
+    client_id: String,
+    /// Reqwest client
+    client: reqwest::Client,
+}
+
+impl Twitch {
+    const BASE_URL: &str = "https://api.twitch.tv/helix";
+
+    pub fn new(token: String, client_id: String, client: Client) -> Self {
+        Self {
+            token,
+            client_id,
+            client,
+        }
+    }
+
+    async fn refresh_token(&mut self) {
+        info!(target: TARGET, "Refreshing token");
+
+        let url = "https://id.twitch.tv/oauth2/token";
+
+        let params = [
+            ("client_id", &self.client_id),
+            (
+                "client_secret",
+                &std::env::var("TWITCH_CLIENT_SECRET")
+                    .expect("Can't refresh without client secret"),
+            ),
+            ("grant_type", &"client_credentials".to_owned()),
+        ];
+        let result = self.client.post(url).form(&params).send().await;
+
+        match result {
+            Ok(response) => {
+                let json = response.json::<Credentials>().await;
+
+                match json {
+                    Ok(credentials) => {
+                        debug!(
+                            target: TARGET,
+                            "New access token: {}", credentials.access_token
+                        );
+                        self.token = credentials.access_token;
+                    }
+                    Err(error) => error!(
+                        target: TARGET,
+                        "Failed to get json data from response: Reason: {}",
+                        error.to_string()
+                    ),
+                }
+            }
+            Err(error) => error!(
+                target: TARGET,
+                "Failed to send request. Reason: {}",
+                error.to_string()
+            ),
+        }
+    }
+
+    async fn validate_token(&mut self) {
+        info!(target: TARGET, "Validating token");
+
+        let url = "https://id.twitch.tv/oauth2/validate";
+
+        let result = self.client.get(url).bearer_auth(&self.token).send().await;
+
+        match result {
+            Ok(response) => match response.status() {
+                StatusCode::UNAUTHORIZED => self.refresh_token().await,
+                StatusCode::OK => {
+                    let json = response.json::<Validation>().await;
+                    match json {
+                        Ok(validation) => {
+                            debug!(
+                                target: TARGET,
+                                "Token expires in: {} seconds", validation.expires_in
+                            );
+                        }
+                        Err(error) => error!(
+                            target: TARGET,
+                            "Failed to get json data from response: Reason: {}",
+                            error.to_string()
+                        ),
+                    }
+                }
+                _ => {}
+            },
+            Err(error) => error!(
+                target: TARGET,
+                "Failed to send request. Reason: {}",
+                error.to_string()
+            ),
+        }
+    }
+
+    /// Fetches the current stream information for the `user_id` provided.
+    ///
+    /// Reference: `https://dev.twitch.tv/docs/api/reference#get-streams`
+    pub async fn get_stream_info(&mut self, user_id: u64) -> Result<StreamInfo, String> {
+        info!(target: TARGET, "Getting stream info for user: {}", user_id);
+
+        self.validate_token().await;
+
+        let url = format!("{}{}", Twitch::BASE_URL, "/streams");
+        let resp = self
+            .client
+            .get(url)
+            .bearer_auth(&self.token)
+            .query(&[("user_id", user_id)])
+            .header("Client-Id", &self.client_id)
+            .send()
+            .await;
+
+        match resp {
+            Ok(response) => {
+                debug!(target: TARGET, "Got response: {:?}", response);
+                let status_code = response.status();
+                if StatusCode::is_success(&status_code) {
+                    debug!(target: TARGET, "Got stream info");
+                    let res = response.json::<StreamInfo>().await;
+                    match res {
+                        Ok(stream_info) => Ok(stream_info),
+                        Err(e) => {
+                            warn!(
+                                target: TARGET,
+                                "Failed parsing response body. Reason: {}",
+                                e.to_string()
+                            );
+                            Err(format!("Failed to get stream info. Reason: {:?}", e))
+                        }
+                    }
+                } else {
+                    let response_text = response.text().await.unwrap_or_else(|err| err.to_string());
+                    warn!(
+                        target: TARGET,
+                        "Failed getting stream info. Reason: {}", response_text
+                    );
+                    Err(format!(
+                        "Failed to get stream info. Reason: {}",
+                        response_text
+                    ))
+                }
+            }
+            Err(error) => {
+                error!(
+                    target: TARGET,
+                    "Error when sending request. Reason: {:?}", error
+                );
+                Err(format!("Failed to get stream info. Reason: {:?}", error))
+            }
+        }
+    }
+}
+
+#[derive(Default, Debug, Deserialize)]
+pub struct StreamData {
+    /// An ID that identifies the stream. You can use this ID later to look up the video on demand (VOD).
+    pub id: String,
+    /// The ID of the user that’s broadcasting the stream.
+    pub user_id: String,
+    /// The user’s login name.
+    pub user_login: String,
+    /// The user’s display name.
+    pub user_name: String,
+    /// The ID of the category or game being played.
+    pub game_id: String,
+    /// The name of the category or game being played.
+    pub game_name: String,
+    /// The type of stream. Possible values are:
+    /// * live
+    /// If an error occurs, this field is set to an empty string.
+    #[serde(rename = "type")]
+    pub stream_type: String,
+    /// The stream’s title. Is an empty string if not set.
+    pub title: String,
+    /// The tags applied to the stream.
+    pub tags: Vec<String>,
+    /// The number of users watching the stream.
+    pub viewer_count: u32,
+    /// The UTC date and time (in RFC3339 format) of when the broadcast began.
+    pub started_at: String,
+    /// The language that the stream uses. This is an ISO 639-1 two-letter language code or *other* if the stream uses a language not in the list of [supported stream languages](https://help.twitch.tv/s/article/languages-on-twitch#streamlang).
+    pub language: String,
+    /// A URL to an image of a frame from the last 5 minutes of the stream. Replace the width and height placeholders in the URL (`{width}x{height}`) with the size of the image you want, in pixels.
+    pub thumbnail_url: String,
+    /// A Boolean value that indicates whether the stream is meant for mature audiences.
+    pub is_mature: bool,
+}
+
+#[derive(Default, Debug, Deserialize)]
+pub struct Pagination {
+    /// The cursor used to get the next page of results. Set the request’s *after* or before query parameter to this value depending on whether you’re paging forwards or backwards.
+    cursor: Option<String>,
+}
+
+#[derive(Default, Debug, Deserialize)]
+pub struct StreamInfo {
+    /// The list of streams.
+    data: Vec<StreamData>,
+    /// The information used to page through the list of results. The object is empty if there are no more pages left to page through. [Read More](https://dev.twitch.tv/docs/api/guide#pagination)
+    pagination: Pagination,
+}
+
+#[derive(Default, Debug, Deserialize)]
+struct Credentials {
+    access_token: String,
+    expires_in: u32,
+    token_type: String,
+}
+
+#[derive(Default, Debug, Deserialize)]
+struct Validation {
+    client_id: String,
+    login: Option<String>,
+    scopes: Option<Vec<String>>,
+    user_id: Option<String>,
+    expires_in: u32,
+}
---
+++ server/twitch/src/main.rs
@@ -0,0 +1,20 @@
+use twitch::Twitch;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    dotenv::dotenv().ok();
+
+    let token = std::env::var("TWITCH_APP_ACCESS_TOKEN")
+        .expect("Needs a Twitch app access token to continue");
+    let client_id =
+        std::env::var("TWITCH_CLIENT_ID").expect("Needs a Twitch client id to continue");
+    let client = reqwest::Client::new();
+
+    let user_id = 54605357;
+
+    let mut twitch = Twitch::new(token, client_id, client);
+    let response = twitch.get_stream_info(user_id).await;
+
+    dbg!(response);
+}
