6262fb1a507813fedfe4b507b206e0d95e836e2c
Joxtacy <jesper.hasselquist@pm.me>
2024-03-16 15:04:41 +0900

    feat(chat-display): add support for color of sender

--- chat-display/src/components/chat-message.tsx
+++ chat-display/src/components/chat-message.tsx
@@ -1,14 +1,16 @@
 import type { FC } from "hono/jsx";
+import { RabbitMessage } from "../types";
 
-const ChatMessage: FC<{ message: string; sender: string }> = ({
+const ChatMessage: FC<RabbitMessage> = ({
 	message,
 	sender,
+	color,
 }) => (
 	<div
 		class="chat-message"
 		_="init wait 15s then add .hidden then settle then remove me"
 	>
-		<span class="sender">{sender}</span>
+		<span class="sender" style={{ color }}>{sender}</span>
 		<span class="divider">: </span>
 		<span class="message">{message}</span>
 	</div>
--- chat-display/src/types.tsx
+++ chat-display/src/types.tsx
@@ -4,4 +4,5 @@
 export interface RabbitMessage {
 	message: string;
 	sender: string;
+	color?: string;
 }
