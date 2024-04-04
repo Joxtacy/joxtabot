d562c05e5a4a00a23a85212a302a966156d59a40
Joxtacy <jesper.hasselquist@pm.me>
2024-04-05 00:12:40 +0900

    style(chat-display): `bun run check`

--- chat-display/src/components/chat-message.tsx
+++ chat-display/src/components/chat-message.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "hono/jsx";
-import { Emote, RabbitMessage } from "../types";
+import type { Emote, RabbitMessage } from "../types";
 
 const replaceEmotes = (message: string, emotes: Emote[]) => {
 	const reversed = emotes.toReversed();
@@ -9,30 +9,35 @@ const replaceEmotes = (message: string, emotes: Emote[]) => {
 		const start = emote.charRange[0];
 		const end = emote.charRange[1];
 		const code = emote.id;
-		const url = emote.urlTemplate.replace("{{id}}", emote.id).replace("{{format}}", emote.format).replace("{{theme_mode}}", "dark").replace("{{scale}}", "1.0");
+		const url = emote.urlTemplate
+			.replace("{{id}}", emote.id)
+			.replace("{{format}}", emote.format)
+			.replace("{{theme_mode}}", "dark")
+			.replace("{{scale}}", "1.0");
 		const firstPart = modMsg.slice(0, start);
 		const lastPart = modMsg.slice(end);
 		const emoteComponent = <EmoteComponent src={url} alt={code} />;
 		modMsg = firstPart;
 
 		components = components.slice(1);
-		components = [<Message part={firstPart} />, emoteComponent, <Message part={lastPart} />, ...components];
+		components = [
+			<Message part={firstPart} />,
+			emoteComponent,
+			<Message part={lastPart} />,
+			...components,
+		];
 	}
 	return components;
 };
 
-const Message: FC<{ part: string }> = ({ part }) => {
-	return (
-		// the "white-space: pre" keeps whitespaces at the beginning and end of the message
-		<span style={{ "white-space": "pre" }}>{part}</span>
-	)
-};
+const Message: FC<{ part: string }> = ({ part }) => (
+	// the "white-space: pre" keeps whitespaces at the beginning and end of the message
+	<span style={{ "white-space": "pre" }}>{part}</span>
+);
 
-const EmoteComponent: FC<{ src: string, alt: string }> = ({ src, alt }) => {
-	return (
-		<img src={src} alt={alt} />
-	)
-};
+const EmoteComponent: FC<{ src: string; alt: string }> = ({ src, alt }) => (
+	<img src={src} alt={alt} />
+);
 
 const ChatMessage: FC<RabbitMessage> = ({
 	message,
@@ -50,11 +55,13 @@ const ChatMessage: FC<RabbitMessage> = ({
 			{badges?.map(({ name, iconUrl }) => (
 				<img src={iconUrl} alt={name} />
 			))}
-			<span class="sender" style={{ color }}>{sender}</span>
+			<span class="sender" style={{ color }}>
+				{sender}
+			</span>
 			<span class="divider">:&#20;</span>
 			{messageComponents.map((component) => component)}
 		</div>
-	)
+	);
 };
 
 export default ChatMessage;
