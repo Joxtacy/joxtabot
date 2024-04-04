1dba4007ad1f5e2cf89e2455f9caaccaea808187
Joxtacy <jesper.hasselquist@pm.me>
2024-04-05 00:10:11 +0900

    refactor(chat-display): avoid `dangerouslySetInnerHTML`

--- chat-display/src/components/chat-message.tsx
+++ chat-display/src/components/chat-message.tsx
@@ -3,15 +3,37 @@ import { Emote, RabbitMessage } from "../types";
 
 const replaceEmotes = (message: string, emotes: Emote[]) => {
 	const reversed = emotes.toReversed();
+	let components = [<Message part={message} />];
+	let modMsg = message;
 	for (const emote of reversed) {
 		const start = emote.charRange[0];
 		const end = emote.charRange[1];
 		const code = emote.id;
 		const url = emote.urlTemplate.replace("{{id}}", emote.id).replace("{{format}}", emote.format).replace("{{theme_mode}}", "dark").replace("{{scale}}", "1.0");
-		message = message.slice(0, start) + `<img src="${url}" alt="${code}" />` + message.slice(end);
+		const firstPart = modMsg.slice(0, start);
+		const lastPart = modMsg.slice(end);
+		const emoteComponent = <EmoteComponent src={url} alt={code} />;
+		modMsg = firstPart;
+
+		components = components.slice(1);
+		components = [<Message part={firstPart} />, emoteComponent, <Message part={lastPart} />, ...components];
 	}
-	return message;
-}
+	return components;
+};
+
+const Message: FC<{ part: string }> = ({ part }) => {
+	return (
+		// the "white-space: pre" keeps whitespaces at the beginning and end of the message
+		<span style={{ "white-space": "pre" }}>{part}</span>
+	)
+};
+
+const EmoteComponent: FC<{ src: string, alt: string }> = ({ src, alt }) => {
+	return (
+		<img src={src} alt={alt} />
+	)
+};
+
 const ChatMessage: FC<RabbitMessage> = ({
 	message,
 	sender,
@@ -19,8 +41,7 @@ const ChatMessage: FC<RabbitMessage> = ({
 	badges,
 	emotes,
 }) => {
-
-	const msg = replaceEmotes(message, emotes);
+	const messageComponents = replaceEmotes(message, emotes);
 	return (
 		<div
 			class="chat-message"
@@ -31,7 +52,7 @@ const ChatMessage: FC<RabbitMessage> = ({
 			))}
 			<span class="sender" style={{ color }}>{sender}</span>
 			<span class="divider">:&#20;</span>
-			<span class="message" dangerouslySetInnerHTML={{ __html: msg }}></span>
+			{messageComponents.map((component) => component)}
 		</div>
 	)
 };
